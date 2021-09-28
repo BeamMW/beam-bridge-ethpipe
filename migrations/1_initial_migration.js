9c1891308363248264c3af4eae9ee61266229094
roman-strilets <roman@beam-mw.com>
2021-09-28 22:25:06 +0300

    implemented mgrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
-const Migrations = artifacts.require("Migrations");
+const TestToken = artifacts.require("TestToken")
+const Pipe = artifacts.require("Pipe");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer) {
+  const beamPipeUserCid = "0xc86ba121b745a4200e067a782cbc5d7bce576d0e57512e030094ef09bdafe087";
+  const supply = BigInt(200000000000000000000);
+
+  await deployer.deploy(TestToken, supply);
+
+  const testTokenInstance = await TestToken.deployed();
+
+  await deployer.deploy(Pipe, testTokenInstance.address, beamPipeUserCid);
 };
