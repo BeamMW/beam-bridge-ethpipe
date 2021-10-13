6d909bf420769938b6436fc9bb095636577cbc2b
Alex Starun <alex.starun@beam-mw.com>
2021-10-13 16:07:14 +0200

    Fixed migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,12 +2,12 @@ const TestToken = artifacts.require("TestToken")
 const Pipe = artifacts.require("Pipe");
 
 module.exports = async function (deployer) {
-  const beamPipeUserCid = "0xc86ba121b745a4200e067a782cbc5d7bce576d0e57512e030094ef09bdafe087";
-  const supply = BigInt(200000000000000000000);
+  const relayerAddress = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
+  const supply = BigInt(200000000000000); // 2'000'000 coins
 
   await deployer.deploy(TestToken, supply);
 
   const testTokenInstance = await TestToken.deployed();
 
-  await deployer.deploy(Pipe, testTokenInstance.address, beamPipeUserCid);
+  await deployer.deploy(Pipe, testTokenInstance.address, relayerAddress);
 };
